c8fa56ffe520b8e348acc3a15bca5cfd6c7051a5
gitdoallan <mailtoallan@icloud.com>
2022-01-27 17:15:52 -0300

    requisito 7

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+const showSpeciesLocation = () => [...new Set(data.species.map(({ location }) => location))];
+const locationSpecies = (location) => data.species.filter((specie) => specie.location === location);
+
+const showWithFilter = (residents, sex) => {
+  const animals = sex ? residents.filter((resident) => resident.sex === sex) : residents;
+  return animals.map((animal) => animal.name);
+}
+
+const showWithName = (species, { sorted, sex }) => species.map((specie) => {
+  const names = showWithFilter(specie.residents, sex); 
+  if (sorted) names.sort();
+  return { [specie.name]: names };
+});
+
+const speciesName = (species) => species.map((specie) => specie.name);
+const locationSearch = (location, values) => values.includeNames ? showWithName(locationSpecies(location), values) : speciesName(locationSpecies(location));
+
+const animalMap = (values) => (animalMap, location) => ({...animalMap, [location]: locationSearch(location, values)})
+
+function getAnimalMap(values = {}) {
+  return showSpeciesLocation().reduce(animalMap(values), {});
 }
 
 module.exports = getAnimalMap;
