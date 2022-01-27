571d970f4253810df8d54d7a3532ab61f1a5e311
gitdoallan <mailtoallan@icloud.com>
2022-01-27 02:03:13 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
+const { species } = data;
 
-function countAnimals(animal) {
-  // seu código aqui
+const specieName = (theAnimal) => species.find((theSpecie) => theSpecie.name === theAnimal);
+const theCounter = (acc, theSpecie) => ({...acc, [theSpecie.name]: theSpecie.residents.length});
+const countAnimals = (animal) => {
+  if (!animal) { return species.reduce(theCounter, {}); }
+  const specie = specieName(animal.specie);
+  const residents = animal.sex
+    ? specie.residents.filter((resident) => resident.sex === animal.sex)
+    : specie.residents;
+  return residents.length;
 }
 
 module.exports = countAnimals;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,8 +2,7 @@ const data = require('../data/zoo_data');
 const { employees } = data;
 
 function isManager(id) {
-  const checkManager = employees.some((details) => details.managers.find((checkId) => (checkId === id) ));
-  return checkManager;
+  return employees.some((details) => details.managers.find((checkId) => (checkId === id) ));
 }
 console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
@@ -12,8 +11,7 @@ function getRelatedEmployees(managerId) {
     throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
   }
   const managedEmployees = employees.filter((theEmployee) => theEmployee.managers.includes(managerId));
-  const theManagedNames = managedEmployees.map((theEmployee) => `${theEmployee.firstName} ${theEmployee.lastName}`);
-  return theManagedNames;
+  return managedEmployees.map((theEmployee) => `${theEmployee.firstName} ${theEmployee.lastName}`);
 }
 console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
